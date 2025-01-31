285161a2165fc639121162df86530c2c348200c0
Шарафаддин Таджаддинов <sharaftajaddin@gmail.com>
2025-01-31 16:56:18 +0400

    Created pos-system

---
+++ Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+app.MapGet("/POS", () =>
+{
+    return new { 
+        Name = "SHAPOS", 
+        Price = 60
+        };
+});
+
+app.Run();
